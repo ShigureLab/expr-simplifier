74d035a4348aae6dfd6cedde216f44198485522f
SigureMo <sigure.qaq@gmail.com>
2024-09-02 10:08:43 +0800

    🔖 release: v0.1.1

--- src/expr_simplifier/__init__.py
+++ src/expr_simplifier/__init__.py
@@ -3,9 +3,9 @@ from __future__ import annotations
 
 from expr_simplifier.transforms.auto_simplify import auto_simplify as auto_simplify
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Nyakku Shigure"
-__year__ = "2023"
+__year__ = "2024"
 __project_info__ = {
     "name": __name__,
     "version": __version__,
